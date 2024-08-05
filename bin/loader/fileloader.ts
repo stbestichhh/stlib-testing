da9dc4f0e53ca83a9e5fb0886f82912377b01c8c
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-05 22:45:33 +0300

    deleted logs

--- bin/loader/fileloader.ts
+++ bin/loader/fileloader.ts
@@ -3,11 +3,9 @@ import glob from 'fast-glob';
 
 export class FileLoader {
   public static async loadTestFiles() {
-    console.time('a');
     const files = await glob('**/*.{spec,test}.ts', {
       ignore: ['node_modules/**'],
     });
-    console.timeEnd('a');
 
     await Promise.all(
       files.map(async (file) => {
