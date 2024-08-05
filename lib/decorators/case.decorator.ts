a2bf53a10ca72d7ab4efff8113c1ecb1932d75ce
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-05 21:14:25 +0300

    Case decorator

---
+++ lib/decorators/case.decorator.ts
@@ -0,0 +1,20 @@
+export function Case(caseDescription: string): MethodDecorator {
+  return function (
+    target: object,
+    key: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>,
+  ) {
+    const constructor = target.constructor as any;
+
+    if (!constructor.testCases) {
+      constructor.testCases = [];
+    }
+
+    constructor.testCases.push({
+      methodName: key,
+      caseDescription,
+    });
+
+    return descriptor;
+  };
+}
