a474f468a0292a2ea5063d58bd500a89b3af3c17
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-06 14:32:25 +0300

    fixed path for the fileloader

--- bin/loader/fileloader.ts
+++ bin/loader/fileloader.ts
@@ -3,13 +3,16 @@ import glob from 'fast-glob';
 
 export class FileLoader {
   public static async loadTestFiles() {
+    const projectPath = path.resolve('../../../'); // go out of node_modules
+
     const files = await glob('**/*.{spec,test}.ts', {
       ignore: ['node_modules/**'],
+      cwd: projectPath,
     });
 
     await Promise.all(
       files.map(async (file) => {
-        await import(path.resolve(file));
+        await import(path.join(projectPath, file));
       }),
     );
   }
