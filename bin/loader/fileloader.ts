f2e8428c837e2e00759913d16a7bb2820c4f171f
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-12 12:49:40 +0300

    implemented config

--- bin/loader/fileloader.ts
+++ bin/loader/fileloader.ts
@@ -1,12 +1,18 @@
 import path from 'node:path';
 import glob from 'fast-glob';
+import { Config, ConfigType } from '../config';
 
+//? Commented code is only for testing while developing
 export class FileLoader {
   public static async loadTestFiles() {
     const projectPath = path.resolve('../../../'); // go out of node_modules
+    // const projectPath = path.resolve();
 
-    const files = await glob('**/*.{spec,test}.ts', {
-      ignore: ['node_modules/**'],
+    const config: ConfigType | undefined =
+      await Config.handleConfiguration(projectPath);
+
+    const files = await glob(config?.pattern || '**/*.{spec,test}.ts', {
+      ignore: config?.ignore || ['node_modules/**'],
       cwd: projectPath,
     });
 
