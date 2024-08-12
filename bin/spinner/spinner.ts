888e1eedf386641e91b189418783f92111cb0e0a
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-12 15:38:57 +0300

    removed error handling in spinnerWrapper

--- bin/spinner/spinner.ts
+++ bin/spinner/spinner.ts
@@ -3,12 +3,7 @@ import { createSpinner } from 'nanospinner';
 export const spinner = createSpinner('Loading test files');
 
 export const spinnerWrapper = async (callback: Promise<any> | any) => {
-  try {
     spinner.start();
     await callback();
     spinner.success();
-  } catch (e) {
-    spinner.error();
-    console.error(e);
-  }
 };
