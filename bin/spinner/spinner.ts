5cc1da201d323cd97c78e099fa94f40c165521f6
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-12 15:39:25 +0300

    formatted code

--- bin/spinner/spinner.ts
+++ bin/spinner/spinner.ts
@@ -3,7 +3,7 @@ import { createSpinner } from 'nanospinner';
 export const spinner = createSpinner('Loading test files');
 
 export const spinnerWrapper = async (callback: Promise<any> | any) => {
-    spinner.start();
-    await callback();
-    spinner.success();
+  spinner.start();
+  await callback();
+  spinner.success();
 };
