489b82561fe1353ce9bd977977efb74e44924b9b
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-13 13:19:11 +0300

    yaml parser for config

--- bin/config/config.ts
+++ bin/config/config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { isExists } from '@stlib/utils';
 import fs from 'fs';
+import YAML from 'yaml';
 
 export type ConfigType = {
   pattern?: string;
@@ -8,14 +9,8 @@ export type ConfigType = {
 };
 
 export class Config {
-  private static async readConfiguration(projectPath: string) {
-    const configPath = path.join(projectPath, 'stest.config.json');
-
-    if (await isExists(configPath)) {
-      return await fs.promises.readFile(configPath);
-    }
-    return undefined;
-  }
+  private static configPath: string | undefined;
+  private static useYml: boolean = false;
 
   public static async handleConfiguration(
     projectPath: string,
@@ -23,9 +18,43 @@ export class Config {
     const unparsedConfig = await this.readConfiguration(projectPath);
 
     if (unparsedConfig) {
-      const configuration: ConfigType = JSON.parse(unparsedConfig.toString());
+      return await this.parseConfig(unparsedConfig);
+    }
+
+    return undefined;
+  }
+
+  private static async readConfiguration(projectPath: string) {
+    await this.defineConfigPath(projectPath);
+
+    if (this.configPath) {
+      return await fs.promises.readFile(this.configPath);
+    }
+
+    return undefined;
+  }
+
+  private static async parseConfig(unparsedConfig: string | Buffer) {
+    if (unparsedConfig) {
+      let configuration: ConfigType;
+      if (this.useYml) {
+        configuration = YAML.parse(unparsedConfig.toString());
+      } else {
+        configuration = JSON.parse(unparsedConfig.toString());
+      }
       return { pattern: configuration.pattern, ignore: configuration.ignore };
     }
-    return undefined;
+  }
+
+  private static async defineConfigPath(projectPath: string): Promise<void> {
+    const configPathJson = path.join(projectPath, 'stest.config.json');
+    const configPathYml = path.join(projectPath, 'stest.config.yml');
+
+    if (await isExists(configPathJson)) {
+      this.configPath = configPathJson;
+    } else if (await isExists(configPathYml)) {
+      this.useYml = true;
+      this.configPath = configPathYml;
+    }
   }
 }
