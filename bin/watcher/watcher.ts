f29b0cda77e579d63a59f0a4c1966bd09bdb0520
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-22 17:14:23 +0300

    formatted output

--- bin/watcher/watcher.ts
+++ bin/watcher/watcher.ts
@@ -4,6 +4,18 @@ import { TestRunner } from '../runner';
 import { FileLoader } from '../loader';
 import { spinner, spinnerWrapper } from '../spinner';
 
+type ActionSymbolType = {
+  add: string,
+  unlink: string,
+  change: string,
+}
+
+const actionSymbol: ActionSymbolType = {
+  add: '+',
+  unlink: '-',
+  change: '↻',
+}
+
 export class Watcher {
   private watcher: chokidar.FSWatcher;
   private changedFiles: Set<string> = new Set<string>();
@@ -20,15 +32,15 @@ export class Watcher {
   }
 
   public async start() {
-    this.watcher.once('change', (filePath) => this.onFileChange(filePath));
-    this.watcher.once('add', (filePath) => this.onFileChange(filePath));
-    this.watcher.once('unlink', (filePath) => this.onFileChange(filePath));
+    this.watcher.once('change', (filePath) => this.onFileChange(filePath, 'change'));
+    this.watcher.once('add', (filePath) => this.onFileChange(filePath, 'add'));
+    this.watcher.once('unlink', (filePath) => this.onFileChange(filePath, 'unlink'));
 
     await this.runTests();
   }
 
-  private async onFileChange(filepath: string) {
-    console.log(`File changed: ${filepath}`);
+  private async onFileChange(filepath: string, action: keyof ActionSymbolType) {
+    console.log(`\n${actionSymbol[action]} ${filepath}`);
     this.changedFiles.add(path.join(this.projectPath, filepath));
 
     if (this.changedFiles.size === 0) {
