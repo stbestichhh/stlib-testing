35742e67c6f03e91dd875619c48173c62cd74fee
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-26 18:26:43 +0300

    Fixed typo

--- bin/errorInfo/error.info.ts
+++ bin/errorInfo/error.info.ts
@@ -1,10 +1,10 @@
 export function findWhereErrorHasBeenThrown(e: Error, testClassName: string) {
   const stackLines = e.stack?.split('\n');
   if (stackLines) {
-    const revelantLine = stackLines.find((line) =>
+    const relevantLine = stackLines.find((line) =>
       line.trimStart().startsWith(`at ${testClassName}`),
     );
-    return revelantLine ? `${stackLines[0]}\n${revelantLine}` : stackLines[0];
+    return relevantLine ? `${stackLines[0]}\n${relevantLine}` : stackLines[0];
   }
   return undefined;
 }
