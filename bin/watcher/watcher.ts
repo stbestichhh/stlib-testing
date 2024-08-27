62f5675bb0dee28414c071f5dfae1c337a8647d3
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-27 12:10:38 +0300

    New key bindings, some refactoring

--- bin/watcher/watcher.ts
+++ bin/watcher/watcher.ts
@@ -12,6 +12,8 @@ export class Watcher {
   private watcher: chokidar.FSWatcher;
   private changedFiles: Set<string> = new Set<string>();
   private isCachingEnabled: boolean = true;
+  private isWatching: boolean = true;
+  private showHelp: boolean = true;
   private readonly projectPath = path.resolve('../../../');
 
   constructor(
@@ -68,15 +70,20 @@ export class Watcher {
         [changedFilesArray],
         'Loading test files',
       );
-      this.changedFiles.clear();
       await TestRunner.run();
 
-      console.log('\nR - rerun tests\nQ - Exit\n'.bold);
+      this.showBindingsHelp();
     } catch (e) {
       spinner.error();
       if (e instanceof Error) {
         console.error(e.message);
       }
+    }
+  }
+
+  private showBindingsHelp() {
+    if (this.showHelp) {
+      console.log('\nR - rerun previous tests\nA - run all tests\nP - pause file watching\nT - toggle test caching\nC - clear console\nH - show/hide key bindings\nQ - Exit\n'.bold);
     }
   }
 
@@ -87,19 +94,56 @@ export class Watcher {
     }
 
     process.stdin.on('keypress', async (chunk, key) => {
-      if (key && key.name === 'r') {
-        await this.runTests();
-      }
-
-      if (key && key.name === 'q') {
-        exit(0);
+      if (key) {
+        await this.handleKeyAction(key.name);
       }
     });
+  }
+
+  private async handleKeyAction(key: string) {
+    switch (key) {
+      case 'h': this.toggleHelp(); break;
+      case 't': this.toggleCache(); break;
+      case 'p': await this.togglePause(); break;
+      case 'a': await this.rerunAllTests(); break;
+      case 'r': await this.runTests(); break;
+      case 'c': this.clearConsole(); break;
+      case 'q': exit(0); break;
+    }
+  }
+
+  private async rerunAllTests() {
+    this.changedFiles.clear();
+    await this.runTests();
+  }
+
+  private async togglePause() {
+    if (this.isWatching) {
+      await this.watcher.close();
+      this.showToggleInfo(!this.isWatching, 'File watch');
+    } else {
+      await this.start();
+    }
+    this.isWatching = !this.isWatching
+  }
+
+  private toggleCache() {
+    this.isCachingEnabled = !this.isCachingEnabled;
+    this.showToggleInfo(this.isCachingEnabled, 'Cache');
+  }
+
+  private toggleHelp() {
+    this.showHelp = !this.showHelp;
+    this.showToggleInfo(this.showHelp, 'Help');
   }
 
   private clearConsole() {
     console.clear();
     process.stdout.write('\u001b[3J\u001b[2J\u001b[1J');
     console.log('\u001b[0;0H'); // Move cursor to top-left corner
+  }
+
+  private showToggleInfo(switchValue: boolean, switchName: string) {
+    switchValue ? console.log(`${switchName} enabled`.yellow.bold) : console.log(`${switchName} disabled`.yellow.bold);
   }
 }
