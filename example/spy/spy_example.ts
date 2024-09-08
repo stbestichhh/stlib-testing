2cd3ce267e3a4ec8aef8197e7153849a983ff55d
Kiril Yakymchuk <stbestich@gmail.com>
2024-09-08 12:28:07 +0300

    spy example

---
+++ example/spy/spy_example.ts
@@ -0,0 +1,56 @@
+import { assertThat, Case, Mock, spyOn, Test } from "@stlib/testing";
+
+class ExampleClass {
+  greet(name: string) {
+    return `Hello, ${name}!`;
+  }
+}
+
+@Test('Spying Example')
+class SpyingTestSuite {
+
+  @Case('should track function calls')
+  testFunctionSpying() {
+    const example = new ExampleClass();
+    const greetSpy = spyOn(example, 'greet');
+
+    example.greet('Alice');
+    example.greet('Bob');
+
+    assertThat(greetSpy.getCallCount()).toEqual(2);
+    assertThat(greetSpy.wasCalledWith('Alice')).toBeTruthy();
+    assertThat(greetSpy.getCallArgs(1)[0]).toEqual('Bob');
+  }
+
+  @Case('should track the order of function calls')
+  testFunctionCallOrder() {
+    const example = new ExampleClass();
+    const greetSpy = spyOn(example, 'greet');
+
+    example.greet('Alice');
+    example.greet('Bob');
+
+    const callOrder = greetSpy.getCallOrder();
+
+    assertThat(callOrder[0]).toEqual(1);
+    assertThat(callOrder[1]).toEqual(2);
+  }
+
+  @Case('should track errors thrown by the function')
+  testFunctionThrowsError() {
+    const example = new ExampleClass()
+    const spy = spyOn(example, 'greet')
+    const mock = new Mock(example);
+    mock.mockMethod('greet', () => { throw new Error });
+
+
+    let errorCaught = false;
+    try {
+      example.greet('Alice');
+    } catch (e) {
+      errorCaught = true;
+    }
+
+    assertThat(errorCaught).toBeTruthy();
+  }
+}
